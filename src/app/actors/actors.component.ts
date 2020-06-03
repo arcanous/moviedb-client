b974d02fb08ad059e38ee23abea1d2ec1be6a9e1
Harijs Deksnis <harijs.deksnis@gmail.com>
2020-06-03 10:52:28 +0200

    add missing subscription/unsubscription

--- src/app/actors/actors.component.ts
+++ src/app/actors/actors.component.ts
@@ -1,20 +1,25 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { ActorsService } from '@/app/core/actors/actors.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-actors',
   templateUrl: './actors.component.html',
   styleUrls: ['./actors.component.scss']
 })
-export class ActorsComponent implements OnInit {
+export class ActorsComponent implements OnInit, OnDestroy {
 
+  actorsSubscription: Subscription;
   actors;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.data.subscribe(({ actors }) => this.actors = actors);
+    this.actorsSubscription = this.route.data.subscribe(({ actors }) => this.actors = actors);
+  }
+
+  ngOnDestroy(): void {
+    this.actorsSubscription.unsubscribe();
   }
 
 }
